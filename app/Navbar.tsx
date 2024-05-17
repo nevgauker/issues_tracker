a0d42a8339e031ec3a4b51cf1eef456a37a14ac6
rotem nevgauker <rotemnevgauker@rotems-MacBook-Pro.local>
2024-05-17 12:28:55 +0900

    test3

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -2,38 +2,38 @@
 import Link from 'next/link'
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
-import  { usePathname }from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import classnames from 'classnames'
 const Navbar = () => {
-  const currentPath = usePathname()
+  return <></>
+  //   const currentPath = usePathname()
 
-  const links = [
-    { label:'Dashboard', href:'/' },
-    { label:'Issues', href:'/issues' }
-  ]
+  //   const links = [
+  //     { label:'Dashboard', href:'/' },
+  //     { label:'Issues', href:'/issues' }
+  //   ]
 
-
-  console.log(currentPath)
-  return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/">
-          <AiFillBug className='text-black'/>
-        </Link>
-        <ul className='flex space-x-6'>
-           {links.map(link => 
-           <Link 
-            key={link.href} 
-            className={classnames({
-              'text-zinc-900': link.href === currentPath,
-              'text-zinc-500' : link.href !== currentPath,
-              'hover:text-zinc-800 transition-colors': true
-            })}
-            href={link.href}>{link.label}
-          </Link>
-) }
-        </ul>
-    </nav>
-  )
+  //   console.log(currentPath)
+  //   return (
+  //     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+  //         <Link href="/">
+  //           <AiFillBug className='text-black'/>
+  //         </Link>
+  //         <ul className='flex space-x-6'>
+  //            {links.map(link =>
+  //            <Link
+  //             key={link.href}
+  //             className={classnames({
+  //               'text-zinc-900': link.href === currentPath,
+  //               'text-zinc-500' : link.href !== currentPath,
+  //               'hover:text-zinc-800 transition-colors': true
+  //             })}
+  //             href={link.href}>{link.label}
+  //           </Link>
+  // ) }
+  //         </ul>
+  //     </nav>
+  //   )
 }
 
 export default Navbar
